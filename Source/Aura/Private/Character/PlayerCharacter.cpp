15b29b09ed29cfe50a821f64a28d66ef06b339fe
Sharon Nir <snir.bender@gmail.com>
2025-07-04 20:10:13 -0400

    Configured Game Mode and Player Camera/Rotation

--- Source/Aura/Private/Character/PlayerCharacter.cpp
+++ Source/Aura/Private/Character/PlayerCharacter.cpp
@@ -3,3 +3,18 @@
 
 #include "Character/PlayerCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	// Setup Character Movement Settings
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0F, 400.0F, 0.0F);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// Disable Controller Rotation
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Primordial Zero
+
+
+#include "Game/AuraGameModeBase.h"
+
