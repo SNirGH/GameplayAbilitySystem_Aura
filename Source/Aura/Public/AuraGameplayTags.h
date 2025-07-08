86d6d22e5b2e271ee99f17cd336bbe178b713fc0
Sharon Nir <snir.bender@gmail.com>
2025-07-08 17:08:32 -0400

    Created Asset Manager and Gameplay Tags Singletons

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,22 @@
+﻿// Copyright Primordial Zero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+private:
+	static FAuraGameplayTags GameplayTags;
+};
