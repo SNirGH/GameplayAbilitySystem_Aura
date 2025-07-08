3a4b30cddb4ccbcd9212346ce9a01dff076778ef
Sharon Nir <snir.bender@gmail.com>
2025-07-08 18:39:11 -0400

    Created Ability System Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright Primordial Zero
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PlayerController->GetHUD()))
+		{
+			AAuraPlayerState* AuraPlayerState = PlayerController->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
+			UAttributeSet* AttributeSet = AuraPlayerState->GetAttributeSet();
+
+			const FWidgetControllerParams WidgetControllerParams(PlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
+			AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+			return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+
+	return nullptr;
+}
--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,3 +3,12 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
